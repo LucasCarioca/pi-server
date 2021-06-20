3417004b0ce9202c34a9267b713745fdd459633d
Lucas Desouza <lucasfds2777@gmail.com>
2021-06-21 00:10:34 +0100

    remove unused endpoint

--- app/main.py
+++ app/main.py
@@ -23,9 +23,5 @@ def read_climate():
 def read_config():
     return config
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
