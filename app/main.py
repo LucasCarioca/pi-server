96559bab908b3b29d7d2312077e613c33de11bfe
Lucas Desouza <lucasfds2777@gmail.com>
2021-06-21 05:11:48 +0100

    fix method names and add reboot method

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from sensors.humidity_temperature import get_humidity_and_temperature
 from fastapi import FastAPI
 import json
+import os
 
 config_file = open("/home/pi/apps/server/app/config.json")
 config = json.load(config_file)
@@ -10,18 +11,22 @@ app = FastAPI()
 config_file.close()
 
 @app.get("/")
-def read_root():
+def get_node_info():
     #humidity, temperature = get_humidity_and_temperature()
     return {"server": config["name"], "room": config["room"]}
 
 @app.get("/climate")
-def read_climate():
+def get_node_climate():
     temp, humidity = get_humidity_and_temperature()
     return {"temp": temp, "humidity": humidity}
 
 @app.get("/config")
-def read_config():
+def get_node_config():
     return config
+
+@app.get("/reboot")
+def reboot_node():
+    os.system("sudo reboot")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
